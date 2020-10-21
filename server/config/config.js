ed34a9c66a73fbbb2b5f61a732b61b25a5dbe3a9
elopezmar <elopezmar@guanajuato.gob.mx>
2020-10-21 15:00:00 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://udemyMongo:Fallen2902.@cluster0.kr4fi.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
