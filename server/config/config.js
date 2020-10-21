62d9a490cfb3895e77c22dc4945b6d73163c9a47
elopezmar <elopezmar@guanajuato.gob.mx>
2020-10-21 13:50:39 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 //=======================
 process.env.PORT = process.env.PORT || 3000;
+
+//=======================
+// Puerto
+//=======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=======================
+// Puerto
+//=======================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://udemyMongo:LrPIvRcS3AlN7wPG@cluster0.kr4fi.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
