5cfdaaee690f55fc32420f44be3e7638295e9e84
elopezmar <elopezmar@guanajuato.gob.mx>
2020-11-04 14:56:36 -0600

    Google signin

--- server/config/config.js
+++ server/config/config.js
@@ -16,15 +16,17 @@ let urlDB;
 //=======================
 // Vencimiento del Token
 //=======================
-
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
 //=======================
 // SEED del token
 //=======================
-
 process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
 
+//=======================
+// Google Client ID
+//=======================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '27828696842-bk1j6jc4k4034e4gumcosh2p0lga9hiq.apps.googleusercontent.com';
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // configuracion global de rutas
 app.use(require('./routes/index'));
+
+// habilitar la carpeta pucliv
+app.use(express.static(path.resolve(__dirname, '../public')))
 
 mongoose.connect(
     process.env.URLDB,
